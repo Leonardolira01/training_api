51a13cd08fe2e625c77c9d32a0d7b18ae41baa4d
leonardolira01 <leolira177@gmail.com>
2024-04-18 18:06:28 -0300

    update

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,12 +1,13 @@
-const { Router } = require("express");
+const { Router } = require('express')
 
-const NotesController = require("../controllers/NotesController");
+const NotesController = require('../controllers/NotesController')
 
-const notesRoutes = Router();
+const notesRoutes = Router()
 
-const notesController = new NotesController();
+const notesController = new NotesController()
 
-notesRoutes.post("/:user_id", notesController.create);
+notesRoutes.post('/:user_id', notesController.create)
+notesRoutes.get('/:id', notesController.show)
 
-module.exports = notesRoutes;
 
+module.exports = notesRoutes
