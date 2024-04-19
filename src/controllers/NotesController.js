b37252acd9aea73b8ded10270059cd20b162c4c6
leonardolira01 <leolira177@gmail.com>
2024-04-18 23:01:10 -0300

    update

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -33,7 +33,7 @@ class NotesController {
     response.json()
   }
 
-  async show(request, response){
+  async show(request, response) {
     const { id } = request.params;
 
     const note = await knex("notes").where({ id }).first();
@@ -46,6 +46,44 @@ class NotesController {
       links
     });
 
+  }
+
+  async delete(request, response) {
+    const {id } = request.params;
+
+    await knex("notes").where({ id }).delete();
+
+    return response.json();
+  }
+
+  async index(request, response) {
+    const { title, user_id, tags } = request.query;
+
+    let notes;
+
+    if(tags){
+      const filterTags = tags.split(',').map(tag => tag.trim());
+
+      notes = await knex("tags")
+        .select([
+          "note.id",
+          "note.title",
+          "notes.user_id",
+        ])
+        .where("notes.user_id", user_id)
+        .whereLike("notes.title", `%${title}%`)
+        .whereIn("name", filterTags)
+        .innerJoin("notes", "notes.id", "tags.note_id" )
+        .orderBy("notes.title")
+
+    } else {
+      notes = await knex("notes")
+        .where({ user_id })
+        .whereLike("title", `%${title}%`)
+        .orderBy("title");
+    }
+    
+    return response.json(notes);
   }
 }
 
